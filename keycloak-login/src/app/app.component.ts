279285add92b746036d01ded05400eea496bc91a
Louis FALCE <louis.falce@etu.imt-nord-europe.fr>
2023-03-02 15:17:21 +0100

    front is working and right logo is placed

--- keycloak-login/src/app/app.component.ts
+++ keycloak-login/src/app/app.component.ts
@@ -3,27 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
     <div style="text-align:center" class="content">
       <h1>
-        Welcome to {{title}}!
+        Bienvenue sur {{title}}!
       </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
+      <span style="display: block">{{ title }} application de test</span>
+<br>
+      <img width="200" alt="Angular Logo" src="https://upload.wikimedia.org/wikipedia/fr/thumb/0/05/Logo_Universit%C3%A9_catholique_de_Lille.svg/langfr-800px-Logo_Universit%C3%A9_catholique_de_Lille.svg.png">
+    <h2>Test de connexion avec keycloak ici </h2>
     <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
+        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">connexion</a></h2>
     </ul>
     <router-outlet></router-outlet>
+    </div>
   `,
   styles: []
 })
